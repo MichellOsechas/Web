f7adf5769053f2ab6519345bb5829bbfa73aac75
MichellOsechas <90647320+MichellOsechas@users.noreply.github.com>
2023-03-24 20:52:53 -0500

    Publicación

---
+++ controller/scripts.php
@@ -0,0 +1,4 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+<!-- Start of HubSpot Embed Code -->
+<script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/24339032.js"></script>
+<!-- End of HubSpot Embed Code -->
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php
+include 'controller\head.php';
+?>
+<body>
+    
+<?php
+include 'views\navbar.php'
+?>
+<?php
+include 'views\carrusel.php'
+?>
+
+</body>
+<?php
+include 'controller\scripts.php';
+?>
+</html>
